ca67d8a18fc71471c0e40f95dbb40570f7b7d9fd
Leandro Martins Guimarães <leandropl@gmail.com>
2021-05-11 23:16:23 -0300

    Initial commit

---
+++ bash/install-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Server update
+apt update
+apt upgrade -y
+
+# Docker
+# Ref.: https://docs.docker.com/engine/install/ubuntu/
+
+# Older versions of Docker were called docker, docker.io, or docker-engine. If these are installed, uninstall them
+apt-get remove -y docker docker-engine docker.io containerd runc
+
+# install packages to allow apt to use a repository over HTTPS
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Use the following command to set up the stable repository
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update the apt package index, and install the latest version of Docker Engine and containerd
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
